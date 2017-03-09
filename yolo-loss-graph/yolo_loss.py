4ed860cdfa18cb89ca1028fc8fe2bf17289f0127
Arian Giles García <arian2822@gmail.com>
2017-03-09 12:52:33 -0300

    Added average loss

--- yolo-loss-graph/yolo_loss.py
+++ yolo-loss-graph/yolo_loss.py
@@ -1,6 +1,8 @@
 import os
+import sys
 import argparse
-import matplotlib.pyplot as plt
+from matplotlib import style
+from matplotlib import pyplot as plt
 
 
 # Parse arguments
@@ -22,21 +24,32 @@ if not os.path.isfile(args.log_file):
 loss_data = []
 loss_data.append([])
 loss_data.append([])
+loss_data.append([])
+
 
 with open(args.log_file, 'r') as f:
     for line in f:
         tokens = line.split(":")
         if tokens[0].isdigit():
+            # Iteration number
             loss_data[0].append(int(tokens[0]))
 
             tokens = tokens[1].split(",")
+
+            # Loss
             loss_data[1].append(float(tokens[0]))
+
+            # Average loss
+            loss_data[2].append(float(tokens[1].split()[0]))
 
 
 # Graph loss
-plt.plot(loss_data[0], loss_data[1])
+style.use('ggplot')
+plt.plot(loss_data[0], loss_data[1], label='Loss')
+plt.plot(loss_data[0], loss_data[2], label='Average loss')
 plt.xlabel('No. Iterations')
 plt.ylabel('Loss')
+plt.legend()
 
 
 # Save file if output argument is present
