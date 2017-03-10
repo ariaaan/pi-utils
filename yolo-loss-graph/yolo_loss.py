d91505d325e9a9953cbcb436bd549fda83138399
Arian Giles García <arian2822@gmail.com>
2017-03-10 12:32:33 -0300

    Fixed bug with iterations and resume training

--- yolo-loss-graph/yolo_loss.py
+++ yolo-loss-graph/yolo_loss.py
@@ -26,27 +26,35 @@ loss_data.append([])
 loss_data.append([])
 loss_data.append([])
 
+last_iteration = 0
 
 with open(args.log_file, 'r') as f:
     for line in f:
         tokens = line.split(":")
         if tokens[0].isdigit():
             # Iteration number
-            loss_data[0].append(int(tokens[0]))
+            iteration = int(tokens[0])
 
-            tokens = tokens[1].split(",")
+            if iteration > last_iteration:
+                last_iteration = iteration
+                
+                # Iteration
+                loss_data[0].append(iteration)
 
-            # Loss
-            loss_data[1].append(float(tokens[0]))
+                # Split with commas
+                tokens = tokens[1].split(",")
 
-            # Average loss
-            loss_data[2].append(float(tokens[1].split()[0]))
+                # Loss
+                loss_data[1].append(float(tokens[0]))
+
+                # Average loss
+                loss_data[2].append(float(tokens[1].split()[0]))
 
 
 # Graph loss
 style.use('ggplot')
-plt.plot(loss_data[0], loss_data[1], label='Loss')
-plt.plot(loss_data[0], loss_data[2], label='Average loss')
+plt.plot(loss_data[0], loss_data[1], color='#e74c3c', label='Loss')
+plt.plot(loss_data[0], loss_data[2], color='#f1c40f', label='Average loss')
 plt.xlabel('No. Iterations')
 plt.ylabel('Loss')
 plt.legend()
