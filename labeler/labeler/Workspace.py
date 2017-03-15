ee8550fe55e17e85cd39e8080148d58a580fc87d
Arian Giles García <arian2822@gmail.com>
2017-03-15 16:59:17 -0300

    Changed label filename format

--- labeler/labeler/Workspace.py
+++ labeler/labeler/Workspace.py
@@ -72,6 +72,7 @@ class Scene(QGraphicsScene):
 
     def saveLabels(self):
         if self._label_path != None:
+            self._label_path = self._label_path[:-8] + ".txt"
             lf = QSaveFile(self._label_path)
             if not lf.open(QIODevice.WriteOnly | QIODevice.Text):
                 raise IOError('Cannot open "{}" for writing'.format(self._label_path))
@@ -151,7 +152,7 @@ class Scene(QGraphicsScene):
     def loadImage(self, image_path, label_path):
         self.clear()
         self._image = QPixmap(image_path)
-        self._label_path = label_path
+        self._label_path = label_path[:-8] + ".txt"
         self.setSceneRect(QRectF(self._image.rect()))
         self.addPixmap(self._image)
         lines = []
